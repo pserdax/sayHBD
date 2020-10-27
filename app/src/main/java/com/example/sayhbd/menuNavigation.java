bf8b17ebd559fab419c8ca376d1da70d522856df
pserdax <parahatgeldiyew.s@gmail.com>
2020-10-27 23:33:02 +0200

    Set up Bottom app bar

--- app/src/main/java/com/example/sayhbd/menuNavigation.java
+++ app/src/main/java/com/example/sayhbd/menuNavigation.java
@@ -2,14 +2,25 @@ package com.example.sayhbd;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.sayhbd.databinding.MenuNavigationBinding;
+import com.google.android.material.bottomappbar.BottomAppBar;
+
 
 public class menuNavigation extends Fragment {
+
+    private MenuNavigationBinding binding;
+    private Fragment fragment = null;
+
 
     public menuNavigation() {
         // Required empty public constructor
@@ -22,9 +33,45 @@ public class menuNavigation extends Fragment {
     }
 
     @Override
+    public void onDestroy() {
+        super.onDestroy();
+        binding = null;
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.menu_navigation, container, false);
+        binding = MenuNavigationBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        setSupportActionBar(binding.bottomAppBar);
+        binding.bottomAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.menu_birthday:
+                        fragment = new birthday();
+                        break;
+                    case R.id.menu_settings:
+                        fragment = new settings();
+                        break;
+                    case R.id.saved:
+                        fragment = new liked();
+                        break;
+                }
+                return false;
+            }
+        });
+
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    private void setSupportActionBar(BottomAppBar bottomAppBar) {
     }
 }
